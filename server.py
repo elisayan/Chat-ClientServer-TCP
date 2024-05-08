208781eb858bc9f923c62279e9c9c88f5d7d8154
Elisa Yan <elisa.yan@studio.unibo.it>
2024-05-08 21:22:51 +0200

    first version server

--- server.py
+++ server.py
@@ -1,7 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  8 20:10:49 2024
+import socket
+import threading
 
-@author: jiaax
-"""
+SERVER_HOST = ''
+SERVER_PORT = 12345
+ADDR = (SERVER_HOST,SERVER_PORT)
 
+server_socket = socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(ADDR)
+
+clients = []
+
+def gestione_client(client_socket,client_address):
+    name=client_socket.recv(1024).decode("utf8")
+    welcome_message = "Benvenuto nella chat, "+name+"!"
+    client_socket.send(bytes(welcome_message,"utf8"))
+    
+    broadcast(name+" è entrato nella chat", name)
+    
+    
+    while True:
+        try:
+            message=client_socket.recv(1024).decode("utf8")
+            
+            if message == "{quit}":
+                client_socket.send(bytes("{quit}", "{utf8}"))
+                client_socket.close()
+                rimozione_client(client_socket)
+                broadcast(name + " ha lasciato la chat", name)
+                break
+            else:
+                broadcast(message, name)
+        except:
+            continue    
+
+def broadcast(message, sender_name):
+    for client in clients:
+        if client != server_socket:
+            try:
+                client.send(bytes(sender_name + ": "+message,"utf8"))
+            except:
+                rimozione_client(client)
+
+def rimozione_client(client):
+    if client in clients:
+        clients.remove(client)
+
+while True:
+    client_socket,client_address = server_socket.accept()
+    
+    clients.append(client_socket)
+    
+    client_thread = threading.Thread(target=gestione_client(client_socket, client_address))
+    client_thread.start()
