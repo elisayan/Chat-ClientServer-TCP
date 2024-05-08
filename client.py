50535f0b391379dfc32e2027905a292552bc3768
Elisa Yan <elisa.yan@studio.unibo.it>
2024-05-08 20:12:15 +0200

    initial commit

---
+++ client.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 20:10:54 2024
+
+@author: jiaax
+"""
+
---
+++ server.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 20:10:49 2024
+
+@author: jiaax
+"""
+
