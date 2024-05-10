923601dcb54041d9cc9342d1d121b1d8efe9f367
Elisa Yan <elisa.yan@studio.unibo.it>
2024-05-10 22:24:25 +0200

    first version client

--- client.py
+++ client.py
@@ -1,7 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  8 20:10:54 2024
+import socket
+import threading
+import tkinter as tk
 
-@author: jiaax
-"""
+SERVER_HOST = input("Inserisci il server host: ")
+SERVER_PORT = int(input("Inserisci il server port: ") or 12345)
+ADDR = (SERVER_HOST, SERVER_PORT)
 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(ADDR)
+
+def ricevimento():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode("utf8")
+            message_list.insert(tk.END, message)
+            message_list.see(tk.END)
+        except OSError:
+            break       
+        
+def invio():
+    while True:
+        message = input()
+        client_socket.send(bytes(message,"utf8"))
+
+window = tk.Tk()
+window.title("Chat Client")
+
+message_frame=tk.Frame(window)
+
+my_message = tk.StringVar()
+my_message.set("Scrivi i tuoi messaggi")
+scrollbar = tk.Scrollbar(message_frame)
+
+message_list = tk.Listbox(message_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+message_list.pack(side=tk.LEFT, fill=tk.BOTH)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+message_frame.pack()
+
+entry_field = tk.Entry(window,textvariable=my_message)
+entry_field.bind("<Return>", invio)
+entry_field.pack()
+
+send_button = tk.Button(window, text="Invia", command=invio)
+send_button.pack()
+
+receive_thread = threading.Thread(target=ricevimento)
+receive_thread.start()
+
+window.mainloop()
